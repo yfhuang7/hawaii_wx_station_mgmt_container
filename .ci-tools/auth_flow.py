de502abc9c5c138614000d68158e253f1227a066
mdodge2 <mdodge@hawaii.edu>
2022-04-22 17:13:50 -1000

    add token getter

--- .ci-tools/auth_flow.py
+++ .ci-tools/auth_flow.py
@@ -1,5 +1,14 @@
 #!/bin/python3
+from get_new_tokens import get_new_tokens
 from encrypt_token import encrypt_token
 from update_secret_rest_call import put_secret
+import os
 
-#placeholder
+tokens: dict = get_new_tokens(
+    ike_url=os.environ['ike_url'],
+    ike_key=os.environ['ike_key'],
+    ike_secret=os.environ['ike_secret'],
+    ike_refresh=os.environ['ike_refresh']
+)
+
+# to be continued...
--- .ci-tools/get_new_tokens.py
+++ .ci-tools/get_new_tokens.py
@@ -1,4 +1,20 @@
 #!/bin/python3
-import os, requests
+import requests
 
-#placeholder
+def get_new_tokens(ike_url, ike_key, ike_secret, ike_refresh):
+
+    params = {
+        "grant_type": "refresh_token",
+        "refresh_token": ike_refresh,
+        "client_id": ike_key,
+        "client_secret": ike_secret,
+    }
+
+    r = requests.post(url=ike_url, params=params)
+    # TODO make sure request is happy
+    new_access_token = r.json()['access_token']
+    new_refresh_token = r.json()['refresh_token']
+    return {
+        "access": new_access_token, 
+        "refresh": new_refresh_token
+    }
